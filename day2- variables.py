8b34154863871dbcf7950ac103c858424ce065f2
Guntasneha <snehagunta21@gmail.com>
2026-09-09 17:42:40 +0530

    Variables

---
+++ day2- variables.py
@@ -0,0 +1,98 @@
+DAY 2 
+
+Tokens:- Tokens are small unit in the python.
+
+1. Identifier:- Is a variable or class or function.
+
+variable
+ex:- 
+num='python'
+print(type(num))
+o/p:- str
+
+functions
+ex:-
+def add_(a,b):
+   print(a+b)
+add_(4,5)
+
+class
+ex:-
+class details:
+   pass
+per_1 = details
+
+2. Keywords:- Keywords are already saved in python for specified reason to run.
+
+ex:- if , else , for , while , return , print 
+
+3.literals:- literals are the data type that need to be stored in variables.
+ex:- 
+num(90)
+name = "Sneha"
+
+4. operators:- + , - , =
+
+statements:- statement are the instructions given to the program.
+ex:-
+num = 90
+age = 2
+if age >=18:
+   print(age)
+......
+
+comments:- once comments are open the lines inside will never execute throughtout in python file.
+
+1. Single line comment(#):- it is used to comment only one line.
+ex:- 
+age = 2
+if age >=18: #this check age is greater or equal
+   print(age)
+
+2. Multi line comment(''' ''', """ """):- used to comment more than one line
+ex:- 
+'''
+def add_(a,b):
+   print(a+b)
+add_(4,5)
+'''
+.....
+
+Variable rules:- 
+.can't use number at first position
+.can't use special character anywhere
+.can't use space
+.can't use kewords
+ex:- 
+2num = 90
+$num = 89
+n um=69
+if  =56
+.small letters and capital letters, underscore(_) , 
+ex:-
+num = 90
+Num_= 34
+Sneha_gunta=23
+a = {'name':'sneha','Ac_num': '234567788788'}
+b = {'name':'gunta','Ac_num': '2345677880934'}
+c = {'name':'sindhu','Ac_num': '234567786543'}
+SBI_sneha_details = {'name':'sneha','Ac_num': '234567788788'}
+ICIC_gunta_details = {'name':'sneha','Ac_num': '234567788788'}
+
+
+num = 90
+print(num)
+
+num_3 , num_2 = 56,89
+print(num_3)
+print(num_2)
+
+swapping of two variables 
+ex:-
+a, b = 45,89
+print('a=',a)
+print('b=',b)
+a, b = b, a
+print('a=',a)
+print('b=',b)
+
